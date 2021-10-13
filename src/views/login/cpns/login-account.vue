22df7a035fbe27355ae4b72e99a86daced7414c0
whwh1233 <1249156074@qq.com>
2021-10-13 11:50:59 +0800

    login

--- src/views/login/cpns/login-account.vue
+++ src/views/login/cpns/login-account.vue
@@ -1,3 +1,8 @@
 <template>
-  <div class="login-account">login-account</div>
+  <div class="login-account">
+    <el-form>
+      <el-form-item label="账号"> <el-input></el-input> </el-form-item>
+      <el-form-item label="密码"> <el-input></el-input> </el-form-item>
+    </el-form>
+  </div>
 </template>
--- src/views/login/cpns/login-phone.vue
+++ src/views/login/cpns/login-phone.vue
@@ -1,3 +1,8 @@
 <template>
-  <div class="login-phone">login-phone</div>
+  <div class="login-phone">
+    <el-form>
+      <el-form-item label="手机号"> <el-input></el-input> </el-form-item>
+      <el-form-item label="验证码"> <el-input></el-input> </el-form-item>
+    </el-form>
+  </div>
 </template>
